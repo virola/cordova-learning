89a0e5899adb29d053f746a186d42496bbba02d8
Virola <virola.zhu@gmail.com>
2014-03-03 17:40:13 +0800

    add new plugins to sample

--- sample/platforms/ios/www/cordova_plugins.js
+++ sample/platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,48 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/com.virola.echo/www/echo.js",
+        "id": "com.virola.echo.echo",
+        "clobbers": [
+            "window.plugins.echo"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.console": "0.2.7",
+    "nl.x-services.plugins.socialsharing": "4.0.2",
+    "org.apache.cordova.dialogs": "0.2.6",
+    "com.virola.echo": "1.0.0"
+}
 // BOTTOM OF METADATA
 });
